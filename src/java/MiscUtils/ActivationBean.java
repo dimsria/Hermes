68bb28cb8a2ba8f6dac864bfce2069c239658c02
Dimitrios <dimsria@gmail.com>
2020-09-11 08:46:19 +0200

    La till checkUser funktion.

--- src/java/MiscUtils/ActivationBean.java
+++ src/java/MiscUtils/ActivationBean.java
@@ -5,8 +5,13 @@
  */
 package MiscUtils;
 
+import Abst.UsersFacade;
+import Entities.Users;
 import UserUtils.LoginBean;
-import javax.ejb.Stateless;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -14,23 +19,44 @@ import javax.inject.Named;
  *
  * @author srvmng
  */
-@Stateless@Named(value = "activation")
+@RequestScoped
+@Named(value = "activation")
 public class ActivationBean {
 
   @Inject LoginBean login;
+    
+  @EJB UsersFacade uFacade;
+  
   
   private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  public String activationCode(int count) {
-    StringBuilder builder = new StringBuilder();
-    while (count--!=0) {
-      int character = (int)(Math.random() * ALPHA_NUMERIC_STRING.length());
-      builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+  public String checkUser(){
+      
+    Users u = uFacade.find(login.sentUsername());
+    System.out.println(u.getHasAccess());
+    if (("Yes").equals(u.getHasAccess())){
+     return "activation";   
     }
+    
+    else {
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
+        return null;
+    }
+  }
+  
+  public String activationCode(int count) {
+      
+    
+        StringBuilder builder = new StringBuilder();
+        while (count--!=0) {
+        int character = (int)(Math.random() * ALPHA_NUMERIC_STRING.length());
+        builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
 
-    System.out.print(builder.toString());
-    return builder.toString();
-
+            System.out.print(builder.toString());
+            return builder.toString();
+    }
+ 
   }
 
-}
